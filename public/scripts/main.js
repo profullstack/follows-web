70cdff4b4a6c5efe0b8d4e468b7bb5dca24a72ed
nostrdev <nostrdev@hashbang.sh>
2023-12-24 12:32:40 -0500

    don't await if no relay list

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -182,17 +182,19 @@ function getRelayListEvent(userPubkey, label, success) {
         "limit": 1
     }
     const sub = relays[0].sub([filter]);
+    let output = null;
     sub.on('event', event => {
         let eventValidation = nt.verifySignature(event);
         if (eventValidation !== true) {
             throw new TypeError("We received a fake event!");
         } else {
             print(`Relay list size (${label}): ` + event.tags.length);
-            success(event);
+            output = event;
         }
     });
     sub.on('eose', () => {
         sub.unsub();
+        success(output);
     });
 }
 // wrap function above in a promise
